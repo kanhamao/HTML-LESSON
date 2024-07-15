33f7efd7d75cdb1e51cdf0d551f2399f9e7ad815
kanhamao <maokanha8888@gmail.com>
2024-07-15 15:53:01 +0700

    Add files via upload

---
+++ loops/ex2.py
@@ -0,0 +1,5 @@
+n = 0
+while n <= 15 :
+    if n % 2 == 1 :
+        print(n)
+    n += 1
---
+++ loops/ex3.py
@@ -0,0 +1,9 @@
+i = 0 
+sum = 0 
+while i <= 15 :
+    if i % 2 == 1 :
+        sum += i
+    i += 1
+print(sum)
+    
+
